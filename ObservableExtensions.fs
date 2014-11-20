c538b8d1cbdace4b1aff725cf668a40dda01fe20
anton-pt <anton.pt@outlookc.om>
2014-11-20 16:48:43 +0000

    Split observable extensions in Endorphin.Core in separate file and added zip2/3/4 functions which zip observables of the same type into tuples.
    Changed PicoScope agent to use integer values for sample intervals and to avoid math error. Added more thorough StreamWorker unit tests and fixed issues relating to status updates.
    
    Changed the RampWorker to not trigger the Completed event if cancellation takes place.
    
    Added log4net logging throughout the application.

---
+++ ObservableExtensions.fs
@@ -0,0 +1,27 @@
+﻿namespace Endorphin.Core
+
+open System
+open System.Reactive.Linq
+open System.Threading
+
+module ObservableExtensions =
+    type Observable with
+        static member zipn (sources : IObservable<'a> list) =
+            Observable.Zip(List.toSeq sources)
+
+        static member zip2 (source1 : IObservable<'a>, source2 : IObservable<'b>) =
+            Observable.Zip(source1, source2, fun item1 item2 -> (item1, item2))
+
+        static member zip3 (source1 : IObservable<'a>, source2 : IObservable<'b>, source3 : IObservable<'c>) =
+            Observable.Zip(source1, source2, source3, fun item1 item2 item3 -> (item1, item2, item3))
+            
+        static member zip4 (source1 : IObservable<'a>, source2 : IObservable<'b>, source3 : IObservable<'c>, source4 : IObservable<'d>) =
+            Observable.Zip(source1, source2, source3, source4, fun item1 item2 item3 item4 -> (item1, item2, item3, item4))
+
+        static member buffer (bufferSize : int) (source : IObservable<'a>) =
+            Observable.Buffer(source, bufferSize)
+
+        static member waitHandleForNext (obs : IObservable<'a>) = 
+            let waitHandle = new ManualResetEvent(false) // create the wait handle
+            obs.Take(1) |> Observable.add (fun _ -> waitHandle.Set() |> ignore) // set the wait handle on the next observable value
+            waitHandle // return the wait handle
