f2d971a9c57b46bb8b3507098c2e736fe660bc8a
anton-pt <anton.pt@outlookc.om>
2015-01-04 16:24:37 +0000

    Added some simple event combinators which are not available in the core library.

---
+++ Endorphin.Core/Event.fs
@@ -0,0 +1,11 @@
+﻿namespace Endorphin.Core
+
+[<AutoOpen>]
+module Event =
+    let concatSeq (source: IEvent<'a seq>) =
+        let event = new Event<_>()
+        source |> Event.add (fun xs -> for x in xs do event.Trigger x)
+        event.Publish
+
+    let collectSeq f =
+        Event.map f >> concatSeq
