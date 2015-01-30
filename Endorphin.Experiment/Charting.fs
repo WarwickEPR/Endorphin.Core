1b7c2557264e6ea198faa1a9110328dd2aeb527a
Anton Tcholako <a.p.tcholakov@warwick.ac.uk>
2015-01-30 12:42:54 +0000

    Changed the way the charting extension methods are declared to a C# friendly syntax.

--- Endorphin.Experiment/Charting.fs
+++ Endorphin.Experiment/Charting.fs
@@ -6,38 +6,39 @@ open System.Reactive.Linq
 open System.Threading
 open FSharp.Charting
 open Endorphin.Core
+open System.Runtime.CompilerServices
 
-[<AutoOpen>]
+[<Extension>]
 /// Provides charting extension members for experiment workflows.
 module ChartingExtensions =
 
-    type CwEprExperimentWorker with
+    [<Extension>]
+    /// Creates a real-time updating chart from the CwEprData event stream with a specified refresh interval.
+    let LiveChartExperiment
+        (experimentWorker : CwEprExperimentWorker,
+         refreshInterval : TimeSpan,
+         [<Optional; DefaultParameterValue("CW EPR experiment")>] title : string,
+         [<Optional; DefaultParameterValue("Magnetic field (T)")>] xTitle : string,
+         [<Optional; DefaultParameterValue("")>] yTitle : string) =
+    
+        // chart the real channel data
+        let reData =
+            (experimentWorker.DataUpdated
+                |> Event.map (fun data -> data.ReSignal()))
+                .Sample(refreshInterval)
+                .ObserveOn(SynchronizationContext.CaptureCurrent()) // observe updates on the current synchronization context so that UI can be updated 
+        let reChart = LiveChart.FastLine(reData, Name="Real signal", Title=title, XTitle=xTitle, YTitle=yTitle)
 
-        /// Creates a real-time updating chart from the CwEprData event stream with a specified refresh interval.
-        member experimentWorker.LiveChart
-            (refreshInterval : TimeSpan,
-             [<Optional; DefaultParameterValue("CW EPR experiment")>] title : string,
-             [<Optional; DefaultParameterValue("Magnetic field (T)")>] xTitle : string,
-             [<Optional; DefaultParameterValue("")>] yTitle : string) =
-        
-            // chart the real channel data
-            let reData =
+        // if quadrature detection is not enabled then only plot the real part of the signal
+        if not experimentWorker.Experiment.QuadratureDetection then reChart
+        else
+            // otherwise also plot the imaginary part
+            let imData =
                 (experimentWorker.DataUpdated
-                    |> Event.map (fun data -> data.ReSignal()))
+                    |> Event.map (fun data -> data.ImSignal()))
                     .Sample(refreshInterval)
-                    .ObserveOn(SynchronizationContext.CaptureCurrent()) // observe updates on the current synchronization context so that UI can be updated 
-            let reChart = LiveChart.FastLine(reData, Name="Real signal", Title=title, XTitle=xTitle, YTitle=yTitle)
+                    .ObserveOn(SynchronizationContext.CaptureCurrent())
+            let imChart = LiveChart.FastLine(imData, Name="Imaginary signal", Title=title, XTitle=xTitle, YTitle=yTitle)
 
-            // if quadrature detection is not enabled then only plot the real part of the signal
-            if not experimentWorker.Experiment.QuadratureDetection then reChart
-            else
-                // otherwise also plot the imaginary part
-                let imData =
-                    (experimentWorker.DataUpdated
-                        |> Event.map (fun data -> data.ImSignal()))
-                        .Sample(refreshInterval)
-                        .ObserveOn(SynchronizationContext.CaptureCurrent())
-                let imChart = LiveChart.FastLine(imData, Name="Imaginary signal", Title=title, XTitle=xTitle, YTitle=yTitle)
-
-                // and combine the plots
-                Chart.Combine [ reChart ; imChart ]
+            // and combine the plots
+            Chart.Combine [ reChart ; imChart ]
