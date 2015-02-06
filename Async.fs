ac4b16869de5ce5d820d5c4643c44cc522acdd68
Anton Tcholako <a.p.tcholakov@warwick.ac.uk>
2015-02-06 10:03:45 +0000

    Endorphin 1.1: major code tidy-up. Added comments throughout except in the CW EPR experiment UI code as this still seems like it could be simplified further.

--- Async.fs
+++ Async.fs
@@ -9,6 +9,57 @@ module AsyncExtensions =
     /// Standard type alias for Microsoft.FSharp.Control.MailboxProcessor agent.
     type Agent<'T> = MailboxProcessor<'T>
 
+    /// MailboxProcessor extensions
+    type MailboxProcessor<'T> with
+
+        /// Provides a mechanism for posting messages with an asynchronous reply channel which will call the
+        /// error continuation of the caller if the failure event is fired before the agent responds to the
+        /// message.
+        member agent.PostAndAsyncReplyFailable (failureEvent : IObservable<exn>) buildMessage = async {
+            // health warning: don't mess with this unless you know what you are doing
+
+            // capture the current cancellation token
+            let! token = Async.CancellationToken
+            
+            // create a an asynchronous workflow from continuations, where the specified continuation function
+            // will be called when the workflow is complete, with either success, error or cancellation
+            return! Async.FromContinuations(fun (cont, econt, ccont) ->
+                
+                // create a cancellation capability which will be used to stop listening for a message if an
+                // error or cancellation occurs
+                let requestCancellationCapability = new CancellationTokenSource()
+                
+                // start another agent internally which wil listen for the first message, indicating either success,
+                // failure or cancellation
+                let continuator = Agent.Start((fun (mailbox : Agent<Choice<'Reply, exn, OperationCanceledException>>) ->
+                    async {
+                        // register a callback with the cancellation token which will post a message indicating
+                        // that the request has been cancelled
+                        use __ = token.Register((fun _ ->
+                            let result = Choice3Of3 (new OperationCanceledException("The opeartion was cancelled."))
+                            mailbox.Post result))
+
+                        // subscribe to the provided failure event
+                        use __ = failureEvent.Subscribe(fun exn -> mailbox.Post(Choice2Of3 exn))
+
+                        // wait for the result
+                        let! message = mailbox.Receive()
+                        
+                        match message with // and depending on the result
+                        | Choice1Of3 reply -> cont reply // call the success continuation
+                        | Choice2Of3 exn -> 
+                            requestCancellationCapability.Cancel() // cancel the request
+                            econt exn // call the failure continuation
+                        | Choice3Of3 exn -> 
+                            requestCancellationCapability.Cancel() // cancel the request
+                            ccont exn })) // or call the cancellation continuation
+
+                // start another asynchronous workflow which will post the supplied message to the agent and post a
+                // message indicating success to the continuator agent when it receives a response
+                Async.Start( async {
+                    let! reply = agent.PostAndAsyncReply buildMessage
+                    continuator.Post(Choice1Of3 reply) }, requestCancellationCapability.Token)) }
+
     type Async with
 
         /// Creates an asynchronous workflow that will be resumed when the specified observables produces a
