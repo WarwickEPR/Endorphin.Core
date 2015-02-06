3192fedf045c4594a070e2decec82b6f33ad78dc
Anton Tcholako <a.p.tcholakov@warwick.ac.uk>
2015-02-06 13:54:21 +0000

    Added some comments to the CW EPR Experiment UI code.

--- Endorphin.TempApp/MainWindow.cs
+++ Endorphin.TempApp/MainWindow.cs
@@ -25,8 +25,9 @@ namespace Endorphin.TempApp
     {
         // TODO: move this into a configuration file
 
+        // create a session for connecting to the magnet controller
         MagnetControllerSession magnetControllerSession = 
-            new MagnetControllerSession("GPIB0::4",
+            new MagnetControllerSession("GPIB0::4", // VISA address
                 new MagnetControllerParameters(
                     staticField: 14.146, // tesla
                     fieldCalibration: -0.002845, // tesla per amp
@@ -38,7 +39,7 @@ namespace Endorphin.TempApp
                     shuntCalibration: 0.400, // volts per amp
                     shuntNoise: 0.100, // volts
                     outputResolution: 16, // bits
-                    calibratedRampRates: new double[] {
+                    calibratedRampRates: new double[] { // amps per sec
                         0.00020, 0.00024, 0.00026, 0.00030, 0.00036, 0.00042, 0.00048, 0.00054, 
                         0.00064, 0.00072, 0.00084, 0.00098, 0.00110, 0.00130, 0.00150, 0.00170,
                         0.0020, 0.0024, 0.0026, 0.0030, 0.0036, 0.0042, 0.0048, 0.0054, 
@@ -49,14 +50,16 @@ namespace Endorphin.TempApp
                         0.64, 0.72, 0.84, 0.98, 1.10, 1.30, 1.50, 1.70, 
                         2.0 }));
 
+        // create a session for connecting to the PicoScope
         PicoScope5000Session picoSession =
-            new PicoScope5000Session("CW336/061", Resolution._14bit);
+            new PicoScope5000Session("CW336/061", Resolution._14bit); // device serial and vertical resolution
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        // when the window loads, connect to the hardware and set up the UI with some default values
         private async void MainWindow_Load(object sender, EventArgs e)
         {
             await magnetControllerSession.ConnectAsync().StartAsTask();
@@ -68,7 +71,7 @@ namespace Endorphin.TempApp
             {
                 rampRate.Items.Add(availableRampRate);
             }
-            rampRate.SelectedIndex = 32;
+            rampRate.SelectedIndex = 32; 
 
             startingField.Minimum = new Decimal(magnetControllerParameters.MinimumField);
             startingField.Maximum = new Decimal(magnetControllerParameters.MaximumField);
@@ -100,7 +103,8 @@ namespace Endorphin.TempApp
             magnetControllerSteps.ValueChanged += this.fieldRange_ValueChanged;
         }
 
-        private IDisposable SetWorkerCallbacks(CwEprExperimentWorker experimentWorker)
+        // subscribe to the experiment worker observables and set stop button click handlers 
+        private IDisposable SubscribeToExperimentObservables(CwEprExperimentWorker experimentWorker)
         {
             var statusChangedSub = experimentWorker.StatusChanged.ObserveOn(SynchronizationContext.Current)
                 .Subscribe(status =>
@@ -121,11 +125,8 @@ namespace Endorphin.TempApp
                     stopAfterScanSub.Dispose();
                 });
         }
-
-        private void SetStatusText(string resultText) {
-            
-        }
         
+        // create a chart control for the experiment worker data and put it in the resultsGroup panel
         private void SetChart(CwEprExperimentWorker experimentWorker)
         {
             var liveChart = new ChartTypes.ChartControl(experimentWorker.LiveChartExperiment(TimeSpan.FromSeconds(1)));
@@ -134,6 +135,7 @@ namespace Endorphin.TempApp
             liveChart.Dock = DockStyle.Fill;
         }
 
+        // set UI controls for a running experiments
         private void SetUiRunning()
         {
             startingField.Enabled = false;
@@ -155,6 +157,7 @@ namespace Endorphin.TempApp
             menuSave.Enabled = false;
         }
 
+        // set UI controls for a finished experiment
         private void SetUiFinished()
         {
             startingField.Enabled = true;
@@ -176,8 +179,10 @@ namespace Endorphin.TempApp
             menuSave.Enabled = true;
         }
 
+        // handle a change to one of the magnetic field range inputs
         private void fieldRange_ValueChanged(object sender, EventArgs e)
         {
+            // remove the other control event handlers temporarily to avoid triggering them
             startingField.ValueChanged -= this.fieldRange_ValueChanged;
             finalField.ValueChanged -= this.fieldRange_ValueChanged;
             magnetControllerSteps.ValueChanged -= this.fieldRange_ValueChanged;
@@ -199,17 +204,20 @@ namespace Endorphin.TempApp
                     smcParams.CurrentForIndex(startingFieldIndex),
                     smcParams.CurrentForIndex(finalFieldIndex));
             
+            // update the experiment duration estimate
             estimatedTimeLabel.Text =
                 string.Format("Estimated duration: {0:0.0} s.",
                     (Decimal.ToInt32(numberOfScans.Value)
                         * Math.Abs(smcParams.FieldForIndex(finalFieldIndex) - smcParams.FieldForIndex(startingFieldIndex))
                         / smcParams.FieldRampRateForIndex(rampRate.SelectedIndex)));
 
+            // re-add the control event handlers again
             startingField.ValueChanged += this.fieldRange_ValueChanged;
             finalField.ValueChanged += this.fieldRange_ValueChanged;
             magnetControllerSteps.ValueChanged += this.fieldRange_ValueChanged;
         }
 
+        // when the ramp rate is changed, update the duration estimate
         private void rampRate_SelectedIndexChanged(object sender, EventArgs e)
         {
             var smcParams = magnetControllerSession.MagnetControllerParameters;
@@ -220,6 +228,7 @@ namespace Endorphin.TempApp
                         / smcParams.CurrentRampRateForIndex(rampRate.SelectedIndex)));
         }
 
+        // when the downsampling is disabled, also disable the downsampling ratio input and enable it otherwise
         private void downsamplingEnabled_CheckedChanged(object sender, EventArgs e)
         {
             if (downsamplingEnabled.Checked)
@@ -232,7 +241,8 @@ namespace Endorphin.TempApp
                 downsamplingRatio.Value = new Decimal(1.0);
             }
         }
-
+        
+        // handle the start button  click
         private async void prepareStartButton_Click(object sender, EventArgs e)
         {
             if (magnetControllerSteps.Value == new Decimal(0.0))
@@ -241,10 +251,12 @@ namespace Endorphin.TempApp
                 return;
             }
             
+            // take control of the hardware
             var magnetControllerParameters = magnetControllerSession.MagnetControllerParameters;
             using (var pico = await picoSession.RequestControlAsync().StartAsTask())
             using (var magnetController = await magnetControllerSession.RequestControlAsync().StartAsTask())
             {
+                // set up the experiment according to the parameters entered on the UI
                 var experiment = new CwEprExperiment(
                     startingFieldIndex: magnetControllerParameters.OutputIndexForField(Decimal.ToDouble(startingField.Value)),
                     finalFieldIndex: magnetControllerParameters.OutputIndexForField(Decimal.ToDouble(finalField.Value)),
@@ -257,42 +269,53 @@ namespace Endorphin.TempApp
                     quadratureDetection: quadratureDetection.Checked,
                     numberOfScans: Decimal.ToInt32(numberOfScans.Value));
 
+                // create the experiment worker and subscribe to its events
                 var experimentWorker = new CwEprExperimentWorker(experiment, magnetController, pico);
-                using (var workerSubs = SetWorkerCallbacks(experimentWorker))
+                using (var workerSubs = SubscribeToExperimentObservables(experimentWorker))
                 {
+                    // disable the UI controls until the experiment finishes and update the chart 
                     SetUiRunning();
                     SetChart(experimentWorker);
-                    experimentWorker.PrepareAndStart();
+                    experimentWorker.PrepareAndStart(); // start the experiment
                     try
                     {
+                        // wait for the experiment to finish (ignore the OperationCanceledException if the experiment is canceled)
                         var result = await experimentWorker.DataUpdated
                             .Catch<CwEprData, OperationCanceledException>(ex => Observable.Empty<CwEprData>())
                             .LastAsync();
-                            
+                        
+                        // when the experiment finishes, set up the menu save button to display the save file dalog if it is clicked
+                        // before the start button is clicked again
                         menuSave.GetClickObservable()
                             .TakeUntil(prepareStartButton.GetClickObservable())
                             .Subscribe(args => ShowSaveDialog(experiment, result));
                     }
                     catch (Exception ex)
                     {
+                        // if an error occurs during the experiment, let the user know and close the form
                         MessageBox.Show(string.Format("Experiment failed due to error: {0}.", ex.Message));
                         Close();
                     }
                 }
 
+                // re-enable the UI controls
                 SetUiFinished();
             }
 
         }
 
+        // show the save file dialog for the experiment
         private void ShowSaveDialog(CwEprExperiment experiment, CwEprData experimentResult)
         {
+            // show dialog to ask the user where to save files
             SaveFileDialog saveDialog = new SaveFileDialog();
             saveDialog.Filter = "Comma-separated value files (*.csv)|*.csv";
             saveDialog.RestoreDirectory = true;
 
+            // if the user clicks ok
             if (saveDialog.ShowDialog() == DialogResult.OK)
             {
+                // write the CSV rows in the experiment result to the specified file
                 Stream dataFileStream;
                 if ((dataFileStream = saveDialog.OpenFile()) != null)
                 {
@@ -306,6 +329,7 @@ namespace Endorphin.TempApp
                     dataFileStream.Close();
                 }
 
+                // also write a txt file describing the experiment parameters in a JSON format
                 Stream descriptionFileStream;
                 if ((descriptionFileStream = File.Open(Path.ChangeExtension(saveDialog.FileName, "txt"), FileMode.Create)) != null)
                 {
@@ -324,6 +348,7 @@ namespace Endorphin.TempApp
             }
         }
 
+        // when the form is closed, also close the connections to the hardware
         private async void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
         {
             await magnetControllerSession.CloseSessionAsync().StartAsTask();
