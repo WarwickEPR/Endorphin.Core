71837e286e1e066dc76968ba3a97a194fd8303cb
Anton Tcholako <anton.pt@outlook.com>
2015-05-08 15:14:33 +0100

    Added error handling based on the choice / asyncChoice computation expressions in ExtCore.

---
+++ Endorphin.Core/ErrorHandling.fs
@@ -0,0 +1,33 @@
+﻿namespace ExtCore.Control
+
+[<AutoOpen>]
+module ErrorHandling =
+    let succeed = Choice1Of2
+    let fail = Choice2Of2
+
+    let (|Success|Failure|) =
+        function
+        | Choice1Of2 s -> Success s
+        | Choice2Of2 f -> Failure f
+
+    type AsyncChoiceReplyChannel<'T, 'Error> = AsyncReplyChannel<Choice<'T, 'Error>>
+
+    type AsyncChoiceBuilder with
+        member __.Bind (choice : Choice<'T, 'Error>, binder : 'T -> AsyncChoice<'U, 'Error>) =
+            async {
+                match choice with
+                | Success s -> return! binder s
+                | Failure f -> return fail f }
+
+        member __.Bind (asyncValue : Async<'T>, binder : 'T -> AsyncChoice<'U, 'Error>) =
+            async {
+                let! value = asyncValue
+                return! binder value }
+
+        member __.ReturnFrom (asyncValue : Async<'T>) =
+            async {
+                let! value = asyncValue
+                return succeed value }
+
+        member __.ReturnFrom (choice : Choice<'T, 'Error>) =
+            choice |> async.Return
