49cbb8035f8c1c4624b139f72a023f06cbc78182
Anton Tcholako <anton.pt@outlook.com>
2015-05-18 15:29:58 +0100

    Added comments to error handling module.

--- ErrorHandling.fs
+++ ErrorHandling.fs
@@ -2,32 +2,40 @@
 
 [<AutoOpen>]
 module ErrorHandling =
+    /// Wraps a value as a success represented by a Choice<'T, 'Error>.
     let succeed = Choice1Of2
+    /// Wraps a value as a failure represented by a Choice<'T, 'Error>.
     let fail = Choice2Of2
 
+    /// Conventional definition for pattern-matching Choice<'T, 'Error> as Success or Failure.
     let (|Success|Failure|) =
         function
         | Choice1Of2 s -> Success s
         | Choice2Of2 f -> Failure f
 
+    /// A handle to a capability to reply to a PostAndReply message with either success or failure.
     type AsyncChoiceReplyChannel<'T, 'Error> = AsyncReplyChannel<Choice<'T, 'Error>>
 
+    // Extensions to AsyncChoice builder which bind and return from Choice<'T, 'Error> and Async<'T> values.
     type AsyncChoiceBuilder with
+
+        /// Binds an expression of type Choice<'T, 'Error>.
         member __.Bind (choice : Choice<'T, 'Error>, binder : 'T -> AsyncChoice<'U, 'Error>) =
             async {
                 match choice with
                 | Success s -> return! binder s
                 | Failure f -> return fail f }
 
+        /// Binds an expression of type Async<'T>.
         member __.Bind (asyncValue : Async<'T>, binder : 'T -> AsyncChoice<'U, 'Error>) =
             async {
                 let! value = asyncValue
                 return! binder value }
 
-        member __.ReturnFrom (asyncValue : Async<'T>) =
-            async {
-                let! value = asyncValue
-                return succeed value }
-
+        /// Returns from an expression of type Choice<'T, 'Error>.
         member __.ReturnFrom (choice : Choice<'T, 'Error>) =
             choice |> async.Return
+            
+        /// Returns from an expression of type Async<'T>.
+        member __.ReturnFrom (asyncValue : Async<'T>) =
+            asyncValue |> Async.map succeed
