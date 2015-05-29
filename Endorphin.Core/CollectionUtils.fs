905bb1af3d736fe4179238436b73e78433bb1aaf
Colin Stephen <C.J.Stephen@warwick.ac.uk>
2015-05-29 16:17:38 +0100

    Add a couple of useful functions on lists, duplicates

---
+++ Endorphin.Core/CollectionUtils.fs
@@ -0,0 +1,21 @@
+﻿namespace Endorphin.Core
+
+open Microsoft.FSharp.Collections
+
+module CollectionUtils =
+
+    let duplicates list =
+      let rec findDuplicates acc =
+        function
+        | []                            -> List.ofSet acc
+        | x::xs when List.contains x xs -> findDuplicates (Set.add x acc) xs
+        | x::xs                         -> findDuplicates acc xs
+
+      findDuplicates Set.empty list
+
+    let rec prettyPrintList =
+        function
+        | [] -> ""
+        | head::[] -> sprintf "%A" head
+        | head::(tail::[]) -> sprintf "%A & %A" head tail
+        | head::tail -> sprintf "%A, %A" head (prettyPrintList tail)
