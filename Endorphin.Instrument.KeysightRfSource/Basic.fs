8b5377a99875b6c2754f0d7908fb8da4739855db
Colin Stephen <C.J.Stephen@warwick.ac.uk>
2015-06-05 09:57:06 +0100

    Add missing file with basic functions

---
+++ Endorphin.Instrument.KeysightRfSource/Basic.fs
@@ -0,0 +1,21 @@
+﻿namespace Endorphin.Instrument.Keysight
+
+[<AutoOpen>]
+module Control =
+
+    let private outputStateKey = ":OUTPUT:STATE"
+    let setOutputState = IO.setOnOffState outputStateKey
+    let queryOutputState = IO.queryOnOffState outputStateKey
+
+    let private cwFrequencyKey = ":FREQUENCY"
+    let setCwFrequency = IO.setFrequency cwFrequencyKey
+    let queryCwFrequency = IO.queryFrequency cwFrequencyKey
+
+    let private cwAmplitudeKey = ":POWER"
+    let setCwAmplitude = IO.setAmplitude cwAmplitudeKey
+    let queryCwAmplitude = IO.queryAmplitude cwAmplitudeKey
+
+    let private phaseKey = ":PHASE"
+    let setPhase = IO.setPhase phaseKey
+    let queryPhase = IO.queryPhase phaseKey
+
