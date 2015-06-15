73ecda4f23571cb3d4c6d8fc9a3a0cb5b709e66a
Anton Tcholako <anton.pt@outlook.com>
2015-06-15 15:00:45 +0100

    Renamed file and moved to namespace Microsoft.Fsharp.Collections.

---
+++ Collections.fs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.FSharp.Collections
+
+[<AutoOpen>]
+module CollectionExtensions =
+    
+    [<RequireQualifiedAccess>]
+    module List =
+        let duplicates list =
+          let rec findDuplicates acc =
+            function
+            | []                            -> List.ofSet acc
+            | x::xs when List.contains x xs -> findDuplicates (Set.add x acc) xs
+            | x::xs                         -> findDuplicates acc xs
+
+          findDuplicates Set.empty list
+
+        let rec prettyPrintList =
+            function
+            | []               -> ""
+            | head::[]         -> sprintf "%A" head
+            | head::(tail::[]) -> sprintf "%A & %A" head tail
+            | head::tail       -> sprintf "%A, %s" head (prettyPrintList tail)
