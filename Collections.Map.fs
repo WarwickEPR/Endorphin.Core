ddd1c18d4e7e4208e75dc844068ab226da691d01
Anton Tcholako <anton.pt@outlook.com>
2015-06-19 12:41:16 +0100

    Moved CollectionUtils to Endorphin.Core.List/Map/etc. Corrected Keysight code accordingly.

---
+++ Collections.Map.fs
@@ -0,0 +1,6 @@
+﻿[<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Endorphin.Core.Map
+
+let findArray keys map = 
+    keys
+    |> Array.map (fun key -> Map.find key map)
