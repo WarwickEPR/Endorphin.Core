373807ad2277ff200cf2ea8045f3b7db2cc934de
Jake Lishman <j.lishman@warwick.ac.uk>
2015-07-09 15:27:40 +0100

    Remove unnecessary Endian module

--- Endorphin.Instrument.KeysightRfSource/Scripts/load-project.fsx
+++ Endorphin.Instrument.KeysightRfSource/Scripts/load-project.fsx
@@ -9,7 +9,6 @@
       @"..\Source.fs"
       @"..\Modulation.fs"
       @"..\Sweep.fs"
+      @"..\IQData.fs"
       @"..\RfSource.fs"
       @"..\DummyInstrument.fs"
-      @"..\Endian.fs"
-      @"..\IQData.fs"
