97c846395e5cb31859907c2252435c1e7a46039c
Jake Lishman <j.lishman@warwick.ac.uk>
2015-07-20 10:57:24 +0100

    Stop tracking VS loading scripts

--- Endorphin.Test.Instrument.KeysightRfSource/Scripts/load-project.fsx
+++
@@ -1,6 +0,0 @@
-// Warning: generated file; your changes could be lost when a new file is generated.
-#I __SOURCE_DIRECTORY__
-#load @"load-references.fsx"
-#load @"..\Utils.fs"
-      @"..\RfSourceMockTest.fs"
-      @"..\RfSourceLiveTest.fs"
