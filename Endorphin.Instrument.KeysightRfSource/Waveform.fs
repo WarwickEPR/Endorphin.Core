f9f3c765fe5932f4abfc5a2ff651e349703ddff1
Jake Lishman <j.lishman@warwick.ac.uk>
2015-07-22 13:44:21 +0100

    Add playing of segments and sequences
    Segments and Sequences can now have their playing triggered, providing the user
    stores them before.

--- Endorphin.Instrument.KeysightRfSource/Waveform.fs
+++ Endorphin.Instrument.KeysightRfSource/Waveform.fs
@@ -145,7 +145,7 @@ module Waveform =
             /// Total filename string for a header file
             let private makeHeaderFileString name = fileNameString headerFolder name
             /// Total filename string for a sequence file
-            let private makeSequenceFileString name = fileNameString sequenceFolder name
+            let makeSequenceFileString name = fileNameString sequenceFolder name
 
             /// Build up a full string for data storage and location
             let private dataStorageString fileName dataString =
@@ -355,13 +355,10 @@ module Waveform =
         let private parallelizeStorage store instrument sequence =
             parallelizeStorageFold "" addErrorStrings store instrument sequence
 
-        /// <summary><para>
         /// Command to write file to volatile memory
-        /// </para><para>
         /// Command reference p.133, p.151. p.133 is technically for the ":MEM:DATA" command rather
         /// than the ":MMEM:DATA" command, but they are identical, and the former has more
         /// information.
-        /// </para></summary>
         let private storeDataKey = ":MMEM:DATA"
         /// Store the three files associated with any segment in the machine's volatile memory.
         /// Returns a StoredSegment type representing the data stored.
@@ -378,33 +375,24 @@ module Waveform =
         let storeSegmentSequence instrument sequence =
             parallelizeStorage storeSegment instrument sequence
 
-        /// <summary><para>
         /// Command to delete all waveform, markers and header files stored in the BBG memory
         /// of the machine (the usual place that they're stored in)
-        /// </para><para>
         /// Command reference p.152
-        /// </para></summary>
         let private deleteAllSegmentsKey = ":MMEM:DEL:WFM"
         /// Delete all the waveform, markers and header files stored in the BBG memory of the
         /// machine (the usual storage location)
         let deleteAllStoredSegments = IO.writeKey deleteAllSegmentsKey
-        /// <summary><para>
         /// Command to delete all sequence files stored in the internal memory of the machine
         /// (the usual storage location).
-        /// </para><para>
         /// Command reference p.146. Uses ":MEM" rather than ":MMEM" for some reason.
-        /// </para></summary>
         let private deleteAllSequencesKey = ":MEM:DEL:SEQ"
         /// Delete all the sequence files stored in the internal memory of the machine (the usual
         /// storage location).
         let deleteAllStoredSequences = IO.writeKey deleteAllSequencesKey
 
-        /// <summary><para>
         /// Command to delete any file on the machine by name.  If a waveform file is passed,
         /// any associated markers and header files are deleted alongside it.
-        /// </para><para>
         /// Command reference p.152
-        /// </para></summary>
         let private deleteFileKey = ":MMEM:DEL:NAME"
         /// Delete a segment from the machine's BBG data storage.  Includes deleting the waveform
         /// file, the markers file and the headers file (if present).
@@ -413,11 +401,8 @@ module Waveform =
         /// location).
         let deleteStoredSequence = IO.setBytesValue fullSequenceFileName deleteFileKey
 
-        /// <summary><para>
         /// Key to store sequences to the machine.
-        /// </para><para>
         /// Command reference p.345
-        /// </para></summary>
         let private storeSequenceKey = ":RAD:ARB:SEQ"
         /// Write a sequence file to the machine and returns the stored sequence type
         let storeSequence instrument sequence = asyncChoice {
@@ -427,3 +412,44 @@ module Waveform =
         /// sequence type
         let storeSequenceSequence instrument sequence =
             parallelizeStorage storeSequence instrument sequence
+
+        /// Key to select a segment or a sequence from the machine
+        /// Command reference p.355
+        let private selectArbFileKey = ":RAD:ARB:WAV"
+        /// Select a segment file on the machine
+        let selectSegment = IO.setBytesValue fullWaveformFileName selectArbFileKey
+        /// Select a sequence file on the machine
+        let selectSequence = IO.setBytesValue fullSequenceFileName selectArbFileKey
+
+        /// Key related to the state of the dual ARB player on the machine. Needs the output
+        /// state to also be on before it will start to play.
+        /// Command reference p.356
+        let private arbStateKey = ":RAD:ARB:STAT"
+        /// Key related to the the modulation state of the RF channels.
+        /// Command reference p.157
+        let private modulationStateKey = ":OUTP:MOD:STAT"
+        /// Key for the overall RF output state. Must be On if anything is to play
+        /// Command reference p.157
+        let private outputStateKey = ":OUTP:STAT"
+
+        /// Set the state of the ARB generator of the given instrument. Can either be On
+        /// or Off.
+        let private setArbState value instrument = asyncChoice {
+            do! IO.setValue onOffStateString arbStateKey instrument value
+            do! IO.setValue onOffStateString modulationStateKey instrument value
+            do! IO.setValue onOffStateString outputStateKey instrument value
+        }
+        /// Turn on the ARB generator of the instrument
+        let turnOnArb = setArbState On
+        /// Turn off the ARB generator of the instrument
+        let turnOffArb = setArbState Off
+
+        /// Begin playing a segment on the instrument
+        let playSegment instrument segment = asyncChoice {
+            do! selectSegment instrument segment
+            do! turnOnArb instrument }
+
+        /// Begin playing a sequence on the instrument
+        let playSequence instrument sequence = asyncChoice {
+            do! selectSequence instrument sequence
+            do! turnOnArb instrument }
