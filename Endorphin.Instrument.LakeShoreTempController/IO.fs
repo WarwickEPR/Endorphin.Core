50dff5cea00c87fea47ac96724ef2d3bb1190e13
Anton Tcholako <anton.pt@outlook.com>
2015-07-29 18:00:50 +0100

    Remove extraneous argument in queryValue and tryQueryValue.

--- Endorphin.Instrument.LakeShoreTempController/IO.fs
+++ Endorphin.Instrument.LakeShoreTempController/IO.fs
@@ -18,18 +18,15 @@ module internal IO =
     /// Performs a query for the value corrseponding to the given key and tries to parse the
     /// result with the given parsing function which returns a Choice<'T, string> indicating
     /// success or failure.
-    let private tryQueryValue tryParseFunc key tempController =
+    let private tryQueryValue tryParseFunc key =
         tryPerformQuery tryParseFunc (sprintf "%s?" key)
 
     /// Performs a query for the value corresponding to the given key and parses the result with
     /// the given parsing function.
-    let private queryValue parseFunc key tempController =
+    let private queryValue parseFunc key =
         performQuery parseFunc (sprintf "%s?" key)
 
     /// Performs a query for the value corresponding to the given key for the given temperature
     /// controller loop and parses the result with the given parsing function.
     let private queryValueForLoop parseFunc key loop tempController =
         performQuery parseFunc (sprintf "%s? %s" key (loopString loop))
-
-
-    
