4b26704f1e4ce1617d55df1d0dedeec16313f3af
Anton Tcholako <anton.pt@outlook.com>
2015-07-29 12:45:07 +0100

    Add Status module with functions which read the status bytes.

---
+++ Endorphin.Instrument.LakeShoreTempController/Status.fs
@@ -0,0 +1,13 @@
+﻿namespace Endorphin.Instrument.LakeShoreTempController
+
+open Endorphin.Core
+
+[<RequireQualifiedAccess>]
+/// Functions for extracting information about the device status from status bytes.
+module internal Status =
+    
+    /// True if a command causes an execution error (e.g. parameter out of range).
+    let executionError (EventStatus status) = Byte.nthBit 4 status
+
+    /// True if an invalid command is sent (e.g. syntax error).
+    let commandError   (EventStatus status) = Byte.nthBit 5 status
