d7cfc7a05f7229520f4ddcd49a14c2acce186a41
Anton Tcholako <anton.pt@outlook.com>
2015-07-29 17:55:02 +0100

    Add module containing VISA command keys.

---
+++ Endorphin.Instrument.LakeShoreTempController/Keys.fs
@@ -0,0 +1,13 @@
+﻿namespace Endorphin.Instrument.LakeShoreTempController
+
+[<RequireQualifiedAccess>]
+/// VISA command keys for instrument.
+module internal Keys =
+    let identity = "*IDN"
+    let standardEventStatus = "*ESE"
+    let controlMode = "CMODE"
+    let heaterOutput = "HTR"
+    let manualHeaterOuptut = "MOUT"
+    let pid = "PID"
+    let setPoint = "SETP"
+    let temperature = "TEMP"
