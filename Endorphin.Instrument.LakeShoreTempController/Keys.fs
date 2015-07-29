16357a61c462bc5b49e5d48cba2e2805ce02816e
Anton Tcholako <anton.pt@outlook.com>
2015-07-29 17:56:43 +0100

    Add comments to Keys module.

--- Endorphin.Instrument.LakeShoreTempController/Keys.fs
+++ Endorphin.Instrument.LakeShoreTempController/Keys.fs
@@ -3,11 +3,27 @@
 [<RequireQualifiedAccess>]
 /// VISA command keys for instrument.
 module internal Keys =
+    
+    /// Device identity.
     let identity = "*IDN"
+    
+    /// Standard event status byte.
     let standardEventStatus = "*ESE"
+
+    /// Control loop mode.
     let controlMode = "CMODE"
+
+    /// Current heater output.
     let heaterOutput = "HTR"
+
+    /// Manual heater output setting.
     let manualHeaterOuptut = "MOUT"
+
+    /// PID settings.
     let pid = "PID"
+
+    /// Temperature set point.
     let setPoint = "SETP"
+
+    /// Current temperature.
     let temperature = "TEMP"
