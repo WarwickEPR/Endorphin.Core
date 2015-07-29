2652402051d89c2370685bbe3a14bedb26a17aff
Anton Tcholako <anton.pt@outlook.com>
2015-07-29 11:18:14 +0100

    Add byte processing utilities module to Endorpin.Core.

---
+++ Endorphin.Core/Byte.fs
@@ -0,0 +1,9 @@
+﻿namespace Endorphin.Core
+
+[<RequireQualifiedAccess>]
+/// Byte-processing utilities
+module Byte = 
+    
+    /// Returns a boolean indicating whether the n-th bit of a byte is high.
+    let nthBit n byte =
+        (1uy <<< n) &&& byte <> 0uy
