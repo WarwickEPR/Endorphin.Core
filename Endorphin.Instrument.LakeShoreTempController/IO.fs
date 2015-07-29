91c92ef441f3fd8c9a5b5a50ab945c715c2f8a64
Anton Tcholako <anton.pt@outlook.com>
2015-07-29 18:16:18 +0100

    Add functions for setting values to the instrument.

--- Endorphin.Instrument.LakeShoreTempController/IO.fs
+++ Endorphin.Instrument.LakeShoreTempController/IO.fs
@@ -28,5 +28,28 @@ module internal IO =
 
     /// Performs a query for the value corresponding to the given key for the given temperature
     /// controller loop and parses the result with the given parsing function.
-    let private queryValueForLoop parseFunc key loop tempController =
+    let private queryValueForLoop parseFunc key tempController loop =
         performQuery parseFunc (sprintf "%s? %s" key (loopString loop))
+
+    /// Queries the standard event status byte of the instrument which indicates command and
+    /// execution errors.
+    let queryStandardEventStatus = queryValue parseStandardEventStatus Keys.standardEventStatus
+
+    /// Sends the provided command string to the instrument and immediately checks the standard
+    /// event status byte for errors.
+    let private performCommand command (TempController tempController) = asyncChoice {
+        command |> Visa.writeString tempController
+        let! status = queryStandardEventStatus (TempController tempController)
+        if Status.commandError   status then return! fail "Invalid command sent to instrument."
+        if Status.executionError status then return! fail "Invalid parameters for command." }
+
+    /// Sets the value corresponding to the given key to the instrument after converting it to a
+    /// string with the provided string conversion function.
+    let private setValue stringFunc key tempController value =
+        performCommand (sprintf "%s %s" key (stringFunc value)) tempController
+
+    /// Sets the value corresponding to the given key for the specified loop to the instrument
+    /// after converting it to a string with the string conversion function.
+    let private setValueForLoop stringFunc key tempController loop value =
+        performCommand (sprintf "%s %s, %s" key (loopString loop) (stringFunc value)) tempController
+
