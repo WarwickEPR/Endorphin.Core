48ebb9d1a34b556e4154839b6fcd6927ef2d3335
Jake Lishman <j.lishman@warwick.ac.uk>
2015-07-30 14:46:18 +0100

    Move crc function to inside hexHash

--- Endorphin.Instrument.KeysightRfSource/Hashing.fs
+++ Endorphin.Instrument.KeysightRfSource/Hashing.fs
@@ -4,11 +4,12 @@ open Endorphin.Core.CRC
 
 module internal Hashing =
     /// Create a hash of a byte array as a hexadecimal string 16 characters long.
-    let private hexHashString arr = sprintf "%016x" (crc64 arr)
+    let private hexHashString = sprintf "%016x"
 
     /// Get a hash of the given type, by converting it to a byte array using the given function
     /// then using some hash function on the result to get an answer.
     let hexHash toByteArray value =
         value
         |> toByteArray
+        |> crc64
         |> hexHashString
