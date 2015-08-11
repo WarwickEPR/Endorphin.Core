5ea1a1bb96fdcecf1844505f1f19e0d818c2018d
Anton Tcholako <anton.pt@outlook.com>
2015-08-11 09:54:44 +0100

    Move collection extensions to a single file as there are relatively few of them.

--- Collections.Map.fs
+++
@@ -1,6 +0,0 @@
-﻿[<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Endorphin.Core.Map
-
-let findArray keys map = 
-    keys
-    |> Array.map (fun key -> Map.find key map)
