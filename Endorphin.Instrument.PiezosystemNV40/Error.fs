61f5e31b534381dd991989c1daf352fb1a65ec2b
Taylor Wheeler <taylor_wheeler@yahoo.co.uk>
2015-08-14 14:37:11 +0100

    Add statusStrings function and (OK | Error) pattern matching to Error module.

---
+++ Endorphin.Instrument.PiezosystemNV40/Error.fs
@@ -0,0 +1,40 @@
+﻿namespace Endorphin.Instrument.PiezosystemNV40
+
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+open Endorphin.Core
+
+module Error =    
+    
+    type Error =
+        | NoError
+        | ActuatorUnplugged
+        | WrongActuatorType
+        | ActuatorShortCircuitFault
+        | Overload
+        | Underload
+        | TemperatureOutofRange
+
+    let errorMap = function
+        | Piezojena.Protocols.Nv40Multi.Nv40MultiErrors.None                      -> NoError
+        | Piezojena.Protocols.Nv40Multi.Nv40MultiErrors.ActuatorNotPluggedIn      -> ActuatorUnplugged
+        | Piezojena.Protocols.Nv40Multi.Nv40MultiErrors.WrongActuatorType         -> WrongActuatorType
+        | Piezojena.Protocols.Nv40Multi.Nv40MultiErrors.ActuatorShortCircuitFault -> ActuatorShortCircuitFault 
+        | Piezojena.Protocols.Nv40Multi.Nv40MultiErrors.Overload                  -> Overload
+        | Piezojena.Protocols.Nv40Multi.Nv40MultiErrors.Underload                 -> Underload
+        | Piezojena.Protocols.Nv40Multi.Nv40MultiErrors.TemperatureOutOfRange     -> TemperatureOutofRange
+        | _                         -> failwithf "Invalid error."
+
+    let statusStrings = function
+        | NoError                   -> "No errors."
+        | ActuatorUnplugged         -> "The actuator is not plugged in."
+        | WrongActuatorType         -> "The actuator is of the incorrect type."
+        | ActuatorShortCircuitFault -> "Actuator short circuit."
+        | Overload                  -> "Value out of range, overshoot."
+        | Underload                 -> "Value out of range, undershoot."
+        | TemperatureOutofRange     -> "Temperature out range."
+        | _                         -> failwithf "Invalid error."
+
+                                                                                                                                         
+    let (|Ok|Error|) = function
+        | Error.NoError -> Ok  
+        | errorCode     -> Error (statusStrings errorCode)
