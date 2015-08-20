becda680f26737e8a1aba3f34c7112afdc62fc0e
Jake Lishman <j.lishman@warwick.ac.uk>
2015-08-20 16:38:58 +0100

    Make List.prettyPrint tail-recursive

--- Endorphin.Core/Collections.fs
+++ Endorphin.Core/Collections.fs
@@ -3,24 +3,25 @@
 [<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module List =
     let duplicates list =
-        let rec findDuplicates acc =
-            function
+        let rec findDuplicates acc = function
             | []                            -> List.ofSet acc
             | x::xs when List.contains x xs -> findDuplicates (Set.add x acc) xs
             | x::xs                         -> findDuplicates acc xs
-
         findDuplicates Set.empty list
 
-    let rec prettyPrint =
-        function
-        | []               -> ""
-        | head::[]         -> sprintf "%A" head
-        | head::(tail::[]) -> sprintf "%A & %A" head tail
-        | head::tail       -> sprintf "%A, %s" head (prettyPrint tail)
-
+    /// Prettily print out a list.
+    let prettyPrint list =
+        let rec loop acc = function
+            | [] -> acc
+            | head :: [] -> sprintf "%s & %A" acc head
+            | head :: (tail :: []) -> sprintf "%s, %A & %A" acc head tail
+            | head :: tail -> loop (sprintf "%s, %A" acc head) tail
+        let (acc, list') =
+            match list with
+            | [] -> ("", [])
+            | head :: tail -> ((sprintf "%A" head), tail)
+        loop acc list'
 
 [<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Map =
-    let findArray keys map = 
-        keys
-        |> Array.map (fun key -> Map.find key map)
+    let findArray keys map = keys |> Array.map (fun key -> Map.find key map)
