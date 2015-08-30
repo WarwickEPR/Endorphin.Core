53148a11e59e121ad17a0112b85f769adaa98c40
Anton Tcholako <anton.pt@outlook.com>
2015-08-30 20:17:05 +0100

    Add comments to device, quantities, channel settings and trigger settings modules

--- Endorphin.Instrument.PicoScope5000/Device.fs
+++ Endorphin.Instrument.PicoScope5000/Device.fs
@@ -4,7 +4,13 @@ open System.Runtime.CompilerServices
 open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
 
 [<RequireQualifiedAccess>]
+/// Functions related to vertical resolution for digital samples. The PicoScope 5000
+/// series uses a variable resolution architecture which allows it to change the
+/// resolution between 8 and 16 bit in exchange for having fewer channels and/or a
+/// lower maximum sampling rate. 
 module Resolution =
+
+    /// Returns the value of the resolution in bits.
     let valueInBits =
         function
         | Resolution_8bit  ->  8<bits>
@@ -13,30 +19,38 @@ module Resolution =
         | Resolution_15bit -> 15<bits>
         | Resolution_16bit -> 16<bits>
 
-    let internal availableChannels =
-        function
+    /// Returns a set of available channels for the given resolution. Note that fewer
+    /// channels may be available depending on the device model, power source and
+    /// acquisition settings.
+    let internal availableChannels = function
         | Resolution_8bit
         | Resolution_12bit 
         | Resolution_14bit -> [ ChannelA ; ChannelB ; ChannelC ; ChannelD ] |> Set.ofList
         | Resolution_15bit -> [ ChannelA ; ChannelB ] |> Set.ofList
         | Resolution_16bit -> [ ChannelA ] |> Set.ofList
 
+    /// Returns the maximum ADC count value for a given resolution.
     let maximumAdcCounts = function
         | Resolution_8bit -> 0x7F00s
         | _               -> 0x7FFFs
 
 [<RequireQualifiedAccess>]
+/// Functions related to device timebase and sample interval.
 module Timebase =
-    let fastestForResolution =
-        function
+    
+    /// The fastest available timebase for a given resolution.
+    let fastestForResolution = function
         | Resolution_8bit  -> 0u
         | Resolution_12bit -> 1u
         | Resolution_14bit
         | Resolution_15bit -> 3u
         | Resolution_16bit -> 4u
 
+    /// Returns the next timebase value, corresponding to a longer sample interval.
     let next index = (index + 1u)
 
+    /// Returns the fastest streaming acquisition sample interval available for a
+    /// given number of enabled channels and vertical resolution.
     let fastestStreamingInterval channelCount resolution =
         match (resolution, channelCount) with
         | (Resolution_12bit, 4)
@@ -57,20 +71,35 @@ module Timebase =
         | _ -> failwithf "Invalid number of channels for resolution %A: %d." resolution channelCount
 
 [<RequireQualifiedAccess>]
+/// Functions related to memory segments.
 module MemorySegment =
+    
+    /// Returns the default memory segment. When the device is started, its memory only
+    /// contains one segment.
     let zero = 0u
+
+    /// Returns the next memory segment.
     let next index = (index + 1u)
 
-
 [<RequireQualifiedAccess>]
+/// Functions related to the device power source.
 module PowerSource =
-    let availableChannels = function
+    
+    /// Returns a set of available channels for the given device power source. Note that
+    /// fewer channels may be available depending on the device resolution, model and other 
+    /// acquisition settings.
+    let internal availableChannels = function
         | MainsPower -> [ ChannelA ; ChannelB ; ChannelC ; ChannelD ] |> Set.ofList
         | UsbPower   -> [ ChannelA ; ChannelB ] |> Set.ofList
 
 [<RequireQualifiedAccess>]
+/// Functions related to the device.
 module Device =
-    let availableChannelsForModel (modelNumber : string) =
+
+    /// Returns a set of available channels for the specified model number. Note that fewer
+    /// channels may be available depending on the device resolution, power source and other
+    /// acquisition settings.
+    let internal availableChannelsForModel (modelNumber : string) =
         match int <| modelNumber.[1].ToString() with
         | 2 -> [ ChannelA ; ChannelB ] |> Set.ofList 
         | 4 -> [ ChannelA ; ChannelB ; ChannelC ; ChannelD ] |> Set.ofList
--- Endorphin.Instrument.PicoScope5000/Quantities.fs
+++ Endorphin.Instrument.PicoScope5000/Quantities.fs
@@ -4,17 +4,31 @@ open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
 open NativeModel
 
 [<RequireQualifiedAccess>]
+/// Functions for creating and manipulating time intervals.
 module Interval =
+
+    /// Creates an integer time interval in femtoseconds.
     let fromFemtoseconds = IntervalInFemtoseconds 
+
+    /// Creates an integer time interval in picoseconds.
     let fromPicoseconds  = IntervalInPicoseconds  
+
+    /// Creates an integer time interval in nanoseconds.
     let fromNanoseconds  = IntervalInNanoseconds  
-    let fromMicroseconds = IntervalInMicroseconds 
+
+    /// Creates an integer time interval in microseconds.
+    let fromMicroseconds = IntervalInMicroseconds
+
+    /// Creates an integer time interval in milliseconds.
     let fromMilliseconds = IntervalInMilliseconds 
+
+    /// Creates an integer time interval in seconds.
     let fromSeconds      = IntervalInSeconds      
     
+    /// Converts the given time interval to a floating point value in seconds.
     let asSeconds =
         function
-        | IntervalInFemtoseconds   interval -> (float interval) * 1e-15<s>
+        | IntervalInFemtoseconds  interval -> (float interval) * 1e-15<s>
         | IntervalInPicoseconds   interval -> (float interval) * 1e-12<s>
         | IntervalInNanoseconds   interval -> (float interval) * 1e-9<s>
         | IntervalInMicroseconds  interval -> (float interval) * 1e-6<s>
@@ -22,6 +36,11 @@ module Interval =
         | IntervalInSeconds       interval -> (float interval) * 1.0<s>
 
 [<RequireQualifiedAccess>]
+/// Functions for creating and manipulating voltages.
 module Voltage =
+    
+    /// Creates a floating point voltage in volts.
     let fromVolts = VoltageInVolts
+
+    /// Creates a voltage corresponding to zero volts.
     let zero = VoltageInVolts 0.0f<V>
