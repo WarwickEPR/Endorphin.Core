1f35411101773a130060f534e74fda7facdaf84f
Jake Lishman <j.lishman@warwick.ac.uk>
2015-09-15 22:08:51 +0100

    Remove commented out code

--- Endorphin.Core/Control.fs
+++ Endorphin.Core/Control.fs
@@ -6,54 +6,6 @@ open System.Threading
 module Utils =
     let defer f = { new System.IDisposable with member __.Dispose() = f () }
 
-(*
-module AsyncChoice =
-    let liftAsync comp = comp |> Async.map succeed
-    let liftChoice (choice : Choice<'a, 'b>) = choice |> async.Return
-    let guard cond err = if cond then succeed () else fail err   
-
-    /// Ignore any success value from an AsyncChoice workflow, but still propagate the failure
-    let ignore input = async {
-        let! choice = input
-        return
-            match choice with
-            | Success s -> succeed ()
-            | Failure f -> fail f }
-
-    /// Build a new AsyncChoice workflow whose Choice1Of2 is the result of apply the mapping function
-    /// to the Choice1Of2 values of the two passed workflows, propagating any failures if necessary.
-    let map2 (mapping : 'A -> 'B -> 'C) a b : AsyncChoice<'C, 'Error> = asyncChoice {
-        let! a' = a
-        let! b' = b
-        return mapping a' b' }
-
-    /// Fold an array of Choice<'T, 'Error> into a single Choice<'T [], 'EState>, depending on a
-    /// folding function to fold the errors into the desried form.
-    let private foldChoices (folder : 'EState -> 'Error -> 'EState)
-                            (state  : 'EState)
-                            (arr    : Choice<'T, 'Error> [])
-                            : Choice<'T [], 'EState> =
-        let rec loop (arr : Choice<_,_> []) index typeacc erroracc =
-            if index = arr.Length then
-                if erroracc = state then
-                    succeed typeacc
-                else
-                    fail erroracc
-
-            else
-                match arr.[index] with
-                | Success s -> loop arr (index+1) (Array.append typeacc [|s|]) erroracc
-                | Failure f -> loop arr (index+1) typeacc (folder erroracc f)
-        loop arr 0 [||] state
-
-    /// Compose a sequence of AsyncChoice workflows into a single workflow which runs all of them in
-    /// parallel, and folds the errors into a single one using a fold function and an initial state.
-    let Parallel state (fold : 'EState -> 'Error -> 'EState) (sequence : AsyncChoice<'T, 'Error> seq)
-                 : AsyncChoice<'T [], 'EState> =
-        async {
-            let! sequence' = sequence |> Async.Parallel
-            return foldChoices fold state sequence' } *)
-
 [<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Async =
     /// Apply a mapping function inside an async workflow.
