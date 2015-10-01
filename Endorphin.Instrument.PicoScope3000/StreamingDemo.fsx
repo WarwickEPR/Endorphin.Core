e97b27676ef0a4d5917465f9d7641509fb4aff05
Jake Lishman <j.lishman@warwick.ac.uk>
2015-10-01 13:41:00 +0100

    Change StreamingDemo to use the PicoTech example
    Streaming now works in the example in Aggregated downsampling mode.

--- Endorphin.Instrument.PicoScope3000/StreamingDemo.fsx
+++ Endorphin.Instrument.PicoScope3000/StreamingDemo.fsx
@@ -1,9 +1,12 @@
 ﻿#r "../Endorphin.Core/bin/Debug/Endorphin.Core.dll"
 #r "../packages/FSharp.Control.Reactive.3.2.0/lib/net40/FSharp.Control.Reactive.dll"
+#r "../packages/Rx-Linq.2.2.5/lib/net45/System.Reactive.Linq.dll"
+#r "../packages/Rx-Interfaces.2.2.5/lib/net45/System.Reactive.Interfaces.dll"
+#r "../packages/Rx-Core.2.2.5/lib/net45/System.Reactive.Core.dll"
 #r "../packages/FSharp.Charting.0.90.12/lib/net40/FSharp.Charting.dll"
 #r "bin/Debug/Endorphin.Instrument.PicoScope3000.dll"
 #r "System.Windows.Forms.DataVisualization.dll"
-#r @"..\packages\log4net.2.0.3\lib\net40-full\log4net.dll"
+#r "../packages/log4net.2.0.3/lib/net40-full/log4net.dll"
 
 open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
 open System
@@ -18,7 +21,7 @@ open Endorphin.Instrument.PicoScope3000
 
 // log4net.Config.BasicConfigurator.Configure()
 
-let form = new Form(Visible = true, TopMost = true, Width = 800, Height = 600)
+let form = new Form(Visible = true, Width = 800, Height = 600)
 let uiContext = SynchronizationContext.Current
 let cts = new CancellationTokenSource()
 
@@ -26,22 +29,25 @@ form.Closed |> Observable.add (fun _ -> cts.Cancel())
 
 let streamingParameters = 
     // define the streaming parameters: 14 bit resolution, 20 ms sample interval, 64 kSample bufffer
-    Streaming.Parameters.create (Interval.fromMilliseconds 20<ms>) (64u * 1024u)
-    |> Streaming.Parameters.enableChannel ChannelA DC Range_500mV Voltage.zero FullBandwidth
-    |> Streaming.Parameters.enableChannel ChannelB DC Range_2V Voltage.zero Bandwidth_20MHz
-    |> Streaming.Parameters.sampleChannels [ ChannelA ; ChannelB ] NoDownsampling
-    |> Streaming.Parameters.withAutoStop 0u 500u // acquire 500 samples after the trigger (approximately 10s)
+    Streaming.Parameters.create (Interval.fromMicroseconds 1<us>) 50000u
+    |> Streaming.Parameters.enableChannel ChannelA DC Range_100mV Voltage.zero FullBandwidth
+    |> Streaming.Parameters.enableChannel ChannelB DC Range_200mV Voltage.zero FullBandwidth
+    |> Streaming.Parameters.sampleChannels [ ChannelA ; ChannelB ] Aggregate
+    |> Streaming.Parameters.withDownsamplingRatio 1000u
+    |> Streaming.Parameters.withAutoStop 0u 1000000000u
 
 let showTimeChart acquisition = async {
     do! Async.SwitchToContext uiContext // add the chart to the form using the UI thread context
         
     let chart = 
         Chart.Combine [ 
-            Streaming.Signal.voltageByTime (ChannelA, NoDownsamplingBuffer) acquisition
+            Streaming.Signal.voltageByTime (ChannelA, AggregateBuffer Maximum) acquisition
+            |> Observable.sample (TimeSpan.FromMilliseconds 100.0)
             |> Observable.observeOnContext uiContext
             |> LiveChart.FastLineIncremental
 
-            Streaming.Signal.voltageByTime (ChannelB, NoDownsamplingBuffer) acquisition
+            Streaming.Signal.voltageByTime (ChannelB, AggregateBuffer Maximum) acquisition
+            |> Observable.sample (TimeSpan.FromMilliseconds 100.0)
             |> Observable.observeOnContext uiContext
             |> LiveChart.FastLineIncremental ]
         |> Chart.WithXAxis(Title = "Time")
@@ -69,6 +75,12 @@ let showChartXY acquisition = async {
     // return to the thread pool context
     do! Async.SwitchToThreadPool () }
 
+let printSampleNumber acquisition =
+    Streaming.Signal.voltageByBlock (ChannelA, AggregateBuffer Minimum) acquisition
+    |> Observable.add (fun arr -> printfn "A min: Got %d samples %A" arr.Length arr)
+    Streaming.Signal.voltageByBlock (ChannelA, AggregateBuffer Maximum) acquisition
+    |> Observable.add (fun arr -> printfn "A max: Got %d samples %A" arr.Length arr)
+
 let printStatusUpdates acquisition =
     Streaming.Acquisition.status acquisition
     |> Observable.add (printfn "%A") // print stream status updates (preparing, streaming, finished...) 
@@ -77,6 +89,7 @@ let experiment picoScope = async {
     // create an acquisition with the previously defined parameters and start it after subscribing to its events
     let acquisition = Streaming.Acquisition.create picoScope streamingParameters
     do! showTimeChart acquisition // use showTimeChart to show X and Y vs T or showXYChart to to plot Y vs X 
+    // do printSampleNumber acquisition
     printStatusUpdates acquisition
 
     let acquisitionHandle = Streaming.Acquisition.startWithCancellationToken acquisition cts.Token
