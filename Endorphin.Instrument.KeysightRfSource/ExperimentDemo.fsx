11a3dfe7f4236d06906aa96727b5906e78b153c1
Jake Lishman <j.lishman@warwick.ac.uk>
2015-10-02 17:44:04 +0100

    Change ExperimentDemo.fsx to spin-echo decay experiment

--- Endorphin.Instrument.KeysightRfSource/ExperimentDemo.fsx
+++ Endorphin.Instrument.KeysightRfSource/ExperimentDemo.fsx
@@ -6,41 +6,60 @@ open Endorphin.Instrument.Keysight
 open Experiment
 open Control
 
-let cycle1 =
-    Phase.empty
-    |> Phase.add (PhaseInRad 0.0<rad>)
-    |> Phase.add (PhaseInDeg 90.0<deg>)
+// a phase for use with RF pulses - this sets I and Q to have the same
+// relative power
+let equalIQ = Phase.empty |> Phase.add (Phase_rad 0.0<rad>)
 
-let cycle2 =
-    Phase.empty
-    |> Phase.addSeq [| for i in 1 .. 2 -> PhaseInDeg (float i * 90.0<deg>) |]
-
+// some defined marker types for easy access
 let marker1 = Markers.empty |> Markers.withMarker1 true
-let markers1And3 = marker1  |> Markers.withMarker3 true
+let marker2 = Markers.empty |> Markers.withMarker2 true
+let marker3 = Markers.empty |> Markers.withMarker3 true
 
+// define the pulse sequence
 let pulses = seq {
-    yield rf cycle1 60u
+    // pi/2 pulse
+    yield rf equalIQ 60u
+    // tau pulse
     yield delayWithIncrement 60u 10u
-    yield trigger marker1
-    yield rfWithIncrement cycle2 10u 10u
-    yield marker markers1And3 20u }
+    // pi pulse
+    yield rf equalIQ 120u
+    // tau pulse
+    yield delayWithIncrement 60u 10u
+    // trigger to begin acquisition
+    yield trigger marker1 }
 
+// define parameters to do with the experiment
 let experiment =
     Experiment.empty
     |> withPulseSeq pulses
     |> withRepetitions 128
     |> withShotRepetitionTime 10e-6<s>
 
+// define the routing of the marker channels
+let routing =
+    Routing.empty
+    |> Routing.withBasebandTrigger1 RouteMarker1
+    |> Routing.withBasebandTrigger2 RouteMarker2
+    |> Routing.withEvent1           RouteMarker3
+    |> Routing.withMarker1Polarity  Positive
+    |> Routing.withMarker2Polarity  Positive
+    |> Routing.withMarker3Polarity  Positive
+
 async {
+    // open the keysight box - set the VISA access string you need here and timeout
     let! keysight = RfSource.openInstrument "TCPIP0::192.168.1.2" 10000<ms>
 
-    do printCompressedExperiment experiment
+    // set the routing correctly
+    do! Routing.set keysight routing
+    // set the triggering of the ARB
+    do! ARB.Trigger.set keysight <| ARB.Trigger.continuous FreeRun
+
+    // store the experiment on the machine
     let! storedExperiment = storeExperiment keysight experiment
 
-    (*
-    do! deleteAllStoredSegments keysight
-    do! deleteAllStoredSequences keysight
-    *)
+    // play the experiment
+    do! playStoredWaveform keysight storedExperiment.StoredExperiment
 
+    // tidy up and close
     do RfSource.closeInstrument |> ignore }
 |> Async.RunSynchronously
