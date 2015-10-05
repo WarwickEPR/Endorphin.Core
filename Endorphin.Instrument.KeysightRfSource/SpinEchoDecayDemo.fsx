c4c3866cde68e386a84906d840bb3b2fac8c3216
Jake Lishman <j.lishman@warwick.ac.uk>
2015-10-05 17:01:34 +0100

    Add try..with block

--- Endorphin.Instrument.KeysightRfSource/SpinEchoDecayDemo.fsx
+++ Endorphin.Instrument.KeysightRfSource/SpinEchoDecayDemo.fsx
@@ -1,5 +1,7 @@
 #r @"..\Endorphin.Core\bin\Debug\Endorphin.Core.dll"
 #r @"bin\Debug\Endorphin.Instrument.KeysightRfSource.dll"
+#r @"..\packages\log4net.2.0.3\lib\net40-full\log4net.dll"
+
 
 // I don't open any modules throughout to demonstrate exactly where each function comes from.
 // Most (if not all) modules may be opened to simplify this, but in general I suggest you don't
@@ -10,6 +12,8 @@
 
 open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
 open Endorphin.Instrument.Keysight
+
+// log4net.Config.BasicConfigurator.Configure ()
 
 // a phase for use with RF pulses - this sets I and Q to have the same
 // relative power
@@ -29,20 +33,24 @@ let pulses = seq {
     // pi/2 pulse
     yield Pulse.rf equalIQ 60u
     // tau pulse
-    yield Pulse.delayWithIncrement 60u 10u
+    yield Pulse.delayWithIncrement 6000u 100u
     // pi pulse
     yield Pulse.rf equalIQ 120u
     // tau pulse
-    yield Pulse.delayWithIncrement 60u 10u
+    yield Pulse.delayWithIncrement 6000u 100u
     // trigger to begin acquisition
     yield Pulse.trigger marker1 }
+
+// There is currently a very large problem with the repetitions / shots per point which has manifested
+// itself.  The number of phases * repetitions * shots per point must be less than (very approximately)
+// 128 or the machine might return the error code "630 - too many segments" when you try to play back
+// the experiment :(
 
 // define parameters to do with the experiment
 let experiment =
     Experiment.empty
     |> Experiment.withPulseSeq pulses
     |> Experiment.withRepetitions 128
-    |> Experiment.withShotsPerPoint 128us
     |> Experiment.withShotRepetitionTime 10e-6<s>
 
 // define the routing of the marker channels
@@ -56,30 +64,33 @@ let routing =
     |> Routing.withMarker2Polarity  Positive
     |> Routing.withMarker3Polarity  Positive
 
-async {
-    // open the keysight box - set the VISA access string you need here and timeout
-    let! keysight = RfSource.openInstrument "TCPIP0::192.168.1.2" 10000<ms>
+try
+    async {
+        // open the keysight box - set the VISA access string you need here and timeout
+        let! keysight = RfSource.openInstrument "USB0::0x0957::0x1F01::MY53051252::INSTR" 100000<ms>
 
-    // set the routing correctly
-    do! Routing.set keysight routing
-    // set the triggering of the ARB
-    do! ARB.Trigger.set keysight <| ARB.Trigger.continuous FreeRun
+        // set the routing correctly
+        do! Routing.set keysight routing
+        // set the triggering of the ARB
+        do! ARB.Trigger.set keysight <| ARB.Trigger.continuous FreeRun
 
-    // set the frequency of the carrier wave
-    do! setCarrierFrequency keysight <| Frequency_Hz 150e6<Hz>
-    // set the power of the carrier wave
-    do! setCarrierAmplitude keysight <| Power_dBm 4.0<dBm>
+        // set the frequency of the carrier wave
+        do! setCarrierFrequency keysight <| Frequency_Hz 150e6<Hz>
+        // set the power of the carrier wave
+        do! setCarrierAmplitude keysight <| Power_dBm 4.0<dBm>
 
-    // store the experiment on the machine
-    let! storedExperiment = Control.Experiment.store keysight experiment
+        // store the experiment on the machine
+        let! storedExperiment = Control.Experiment.store keysight experiment
 
-    // if we want to change anything before we begin playback, this tells us which file
-    // holds the experiment - only exists when the library is compiled in debug mode
-    do Control.Print.experimentFile storedExperiment
+        // if we want to change anything before we begin playback, this tells us which file
+        // holds the experiment - only exists when the library is compiled in debug mode
+        do Control.Print.experimentFile storedExperiment
 
-    // play the experiment
-    do! Control.Experiment.playStored keysight storedExperiment
+        // play the experiment
+        do! Control.Experiment.playStored keysight storedExperiment
 
-    // tidy up and close
-    do! RfSource.closeInstrument keysight }
-|> Async.RunSynchronously
+        // tidy up and close
+        do! RfSource.closeInstrument keysight }
+    |> Async.RunSynchronously
+with
+    | :? InstrumentErrorException as exn -> printfn "Failed with instrument errors: %A" exn.Data0
