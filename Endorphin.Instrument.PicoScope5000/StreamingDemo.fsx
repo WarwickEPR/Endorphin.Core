7225a6268294b8cebf2122747fdfab87ed6d4274
Anton Tcholako <anton.pt@outlook.com>
2015-10-19 16:26:47 +0100

    Update PicoScope streaming demo to remove outdated reference to ExtCore.Control

--- Endorphin.Instrument.PicoScope5000/StreamingDemo.fsx
+++ Endorphin.Instrument.PicoScope5000/StreamingDemo.fsx
@@ -1,4 +1,7 @@
 ﻿#r "../Endorphin.Core/bin/Debug/Endorphin.Core.dll"
+#r "../packages/Rx-Core.2.2.5/lib/net45/System.Reactive.Core.dll"
+#r "../packages/Rx-Interfaces.2.2.5/lib/net45/System.Reactive.Interfaces.dll"
+#r "../packages/Rx-Linq.2.2.5/lib/net45/System.Reactive.Linq.dll"
 #r "../packages/FSharp.Control.Reactive.3.2.0/lib/net40/FSharp.Control.Reactive.dll"
 #r "../packages/FSharp.Charting.0.90.12/lib/net40/FSharp.Charting.dll"
 #r "bin/Debug/Endorphin.Instrument.PicoScope5000.dll"
@@ -9,7 +12,6 @@ open System
 open System.Threading
 open System.Windows.Forms
 
-open ExtCore.Control
 open FSharp.Charting
 open FSharp.Control.Reactive
 
