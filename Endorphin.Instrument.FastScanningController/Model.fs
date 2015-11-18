4c2cc564d064ccb90fe98b1dc70383ed819cd248
Ben Green <b.green@warwick.ac.uk>
2015-11-18 13:30:14 +0000

    Initial fast scan controller commit

---
+++ Endorphin.Instrument.FastScanningController/Model.fs
@@ -0,0 +1,25 @@
+﻿namespace Endorphin.Instrument.FastScanningController
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+
+
+[<AutoOpen>]
+module Model =
+    [<AutoOpen>]
+    module Setup = 
+        
+        [<AutoOpen>]
+        module Timing =
+            type DwellTime = decimal<ms>
+
+            type SettlingTime = decimal<us>
+
+        [<AutoOpen>]
+        module Motion = 
+            type PositionCalibration = 
+                { X     : decimal<m/V>
+                  Y     : decimal<m/V>
+                  Z     : decimal<m/V> }
+
+    type ScanningController = internal { PortName : string; Calibration : PositionCalibration }
+
+
