48be264b280d113905f0c5adcd01b9c4c0f8960a
Ben Green <b.green@warwick.ac.uk>
2015-11-19 12:27:04 +0000

    Path utility updated to provide 3D coordinates (though paths are still constrained to 2D)

--- Endorphin.Utilities.Position/Path.fs
+++ Endorphin.Utilities.Position/Path.fs
@@ -1,15 +1,24 @@
 ﻿namespace Endorphin.Utilities.Position
 
+open Point
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+
 module Path =
+    type Plane = 
+        | XY
+        | XZ
+        | YZ
+
     type Path = 
         private { ArrayIndices : (int * int) array
-                  Origin       : float * float
-                  Scale        : float * float }
+                  Origin       : Point
+                  Scale        : decimal<um> * decimal<um>
+                  Plane        : Plane }
 
-    let createSnake origin gridSize stepSize = 
+    let createSnake origin gridSize (stepSize : decimal<um>) plane = 
         let isEven x = (x % 2 = 0)
 
-        let numberOfSteps = int (round((float gridSize) / stepSize))
+        let numberOfSteps = int (round((float gridSize) / (float (stepSize/1.0m<um>))))
         let path = seq {
             for x in 0 .. numberOfSteps do   
                 if isEven x 
@@ -18,23 +27,36 @@ module Path =
 
         { ArrayIndices = path |> Seq.toArray
           Origin = origin
+          Plane = plane
           Scale = (stepSize, stepSize) }
         
-    let createOneDirection origin gridSize stepSize = 
-        let numberOfSteps = int (round((float gridSize) / stepSize))
+    let createOneDirection origin gridSize stepSize plane = 
+        let numberOfSteps = int (round((float gridSize) / (float (stepSize/1.0m<um>))))
         let path = seq {
             for x in 0 .. numberOfSteps do
                 for y in 0 .. numberOfSteps -> (x, y) }
 
         { ArrayIndices = path |> Seq.toArray
           Origin = origin
-          Scale = (stepSize, stepSize) }
+          Scale = (stepSize, stepSize)
+          Plane = plane }
 
-    let coordinateForPoint point path =
-        let (x0, y0) = path.Origin
-        let (x, y) = point
-        let (dx, dy) = path.Scale
-        (x0 + (float x) * dx, y0 + (float y) * dy)
+    let coordinateForPoint (point : int * int) path =
+        let (x0, y0, z0) = path.Origin
+
+        match path.Plane with
+        | XY ->
+            let (x, y) = point
+            let (dx, dy) = path.Scale
+            (x0 + (decimal x) * dx, y0 + (decimal y * dy), z0)
+        | XZ ->
+            let (x, z) = point
+            let (dx, dz) = path.Scale
+            (x0 + (decimal x) * dx, y0, z0 + (decimal z * dz))
+        | YZ ->
+            let (y, z) = point
+            let (dy, dz) = path.Scale
+            (x0, y0 + (decimal y * dy), z0 + (decimal z * dz))
 
     let points path = 
         path.ArrayIndices |> Array.ofSeq
