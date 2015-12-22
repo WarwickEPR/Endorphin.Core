a1cd6cac544c8747c0f111fb7f42d9e681fa188e
Anton Tcholako <anton.pt@outlook.com>
2015-12-22 16:33:52 +0000

    Add load-references and load-project to KeysightRfSource project

---
+++ Endorphin.Instrument.KeysightRfSource/Scripts/load-references-debug.fsx
@@ -0,0 +1,7 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../Endorphin.Core/bin/Debug/Endorphin.Core.dll"
+#r "../../packages/log4net.2.0.3/lib/net40-full/log4net.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
