7574533cd999a4edf0518eca243999db8532c98a
Anton Tcholako <anton.pt@outlook.com>
2015-12-29 13:12:51 +0000

    Change type definitions to in InternalModel.fs to fit style convention

--- Endorphin.Instrument.KeysightRfSource/InternalModel.fs
+++ Endorphin.Instrument.KeysightRfSource/InternalModel.fs
@@ -6,15 +6,15 @@ module internal InternalModel =
     module ARB =
         /// Internal record of an entire recorded segment before being transformed into
         /// machine-readable strings.  Lists are in reverse order for speed.
-        type EncodedSegment = {
-            EncodedIQ      : byte array
-            EncodedMarkers : byte array }
+        type EncodedSegment =
+            { EncodedIQ      : byte array
+              EncodedMarkers : byte array }
 
         /// Segment data after it has been encoded, including the lengths and data indicator '#'.
         /// Ready to write to machine.
-        type EncodedSegmentFiles = {
-            Waveform : byte array
-            Markers  : byte array }
+        type EncodedSegmentFiles =
+            { Waveform : byte array
+              Markers  : byte array }
 
     [<AutoOpen>]
     module Experiment =
@@ -27,21 +27,14 @@ module internal InternalModel =
 
         /// Metadata about the experiment gathered during verification, for use during the
         /// compilation step.
-        type ExperimentMetadata = {
-            /// How many times the experiment is repeated.
-            ExperimentRepetitions : int
-            /// How many pulses there are per phase.
-            PulseCount : int
-            /// How many pulses are RF pulses.
-            RfPulseCount : int
-            /// How many phases there are in the phase cycle.
-            RfPhaseCount : int option
-            /// Which marker to use as the RF blanking marker.
-            RfBlankMarker : UserSignalMarker
-            /// How long to wait before firing the next shot.
-            ShotRepetitionTime : SampleCount
-            /// How many shots to take at each point.
-            ShotsPerPoint : uint16 }
+        type ExperimentMetadata =
+            { ExperimentRepetitions : int
+              PulseCount            : int
+              RfPulseCount          : int
+              RfPhaseCount          : int option
+              RfBlankMarker         : UserSignalMarker
+              ShotRepetitionTime    : SampleCount
+              ShotsPerPoint         : uint16 }
 
         // No need for type aliases here because there's no other step which uses similar types
         /// A single pulse which can be easily converted into a single segment, for use after the
@@ -52,39 +45,38 @@ module internal InternalModel =
             | StaticMarker  of markers : Markers * duration : SampleCount
 
         /// An experiment after it has been passed through the user-input verifier.
-        type VerifiedExperiment = {
-            Pulses : VerifiedPulse list list
-            // double list because we're going to expand into individual lists
-            Metadata : ExperimentMetadata }
+        type VerifiedExperiment =
+            { Pulses   : VerifiedPulse list list // double list because we're going to expand into individual lists
+              Metadata : ExperimentMetadata }
 
-        type CompiledExperimentPoint = {
-            CompiledData : (Sample * SampleCount) list
-            CompiledLength : uint32 }
+        type CompiledExperimentPoint =
+            { CompiledData   : (Sample * SampleCount) list
+              CompiledLength : uint32 }
 
         /// A list of samples and their repetitions, which could be easily written onto the
         /// machine, but likely with a lot of redundancy.
-        type CompiledExperiment = {
-            ExperimentPoints : CompiledExperimentPoint list
-            Metadata : ExperimentMetadata }
+        type CompiledExperiment =
+            { ExperimentPoints : CompiledExperimentPoint list
+              Metadata         : ExperimentMetadata }
 
         /// One element of compression - usually analagous to a single point in the experiment,
         /// with one phase and one duration for the pulses.
-        type CompressedElement = {
-            Element : SequenceElement
-            Segments : Map<string, Segment>
-            Sequences : Map<string, Sequence> }
+        type CompressedElement =
+            { Element   : SequenceElement
+              Segments  : Map<string, Segment>
+              Sequences : Map<string, Sequence> }
 
         /// An experiment inside the compression step.
-        type CompressedExperiment = {
-            Segments : Map<string, Segment>
-            Sequences : Map<string, Sequence>
-            CompressedPoints : Sequence list
-            Metadata : ExperimentMetadata }
+        type CompressedExperiment =
+            { Segments         : Map<string, Segment>
+              Sequences        : Map<string, Sequence>
+              CompressedPoints : Sequence list
+              Metadata         : ExperimentMetadata }
 
         /// An assembled experiment, ready for storing onto the machine.
-        type EncodedExperiment = {
-            Segments : (SegmentId * Segment) list
-            Sequences : (SequenceId * Sequence) list
-            Points : (SequenceId * Sequence) list
-            Experiment : (SequenceId * Sequence)
-            Metadata : ExperimentMetadata }
+        type EncodedExperiment =
+            { Segments   : (SegmentId * Segment) list
+              Sequences  : (SequenceId * Sequence) list
+              Points     : (SequenceId * Sequence) list
+              Experiment : (SequenceId * Sequence)
+              Metadata   : ExperimentMetadata }
